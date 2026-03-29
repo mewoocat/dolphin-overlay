c779bf7f7b2d5b4dcd4cd99af20c5feff5bb0b07
Marcelo Garlet Milani <mgmilani@pm.me>
2026-03-29 18:45:17 +0900

    Avoid recompilation.
    Use writeShellScriptBin instead of wrapProgram to create a derivation which
    only has the desired script calling dolphin.

--- default.nix
+++ default.nix
@@ -11,13 +11,10 @@
 
 final: prev: {
   kdePackages = prev.kdePackages.overrideScope (kfinal: kprev: {
-    dolphin = kprev.dolphin.overrideAttrs (oldAttrs: {
-      nativeBuildInputs = (oldAttrs.nativeBuildInputs or []) ++ [ prev.makeWrapper ];
-      postInstall = (oldAttrs.postInstall or "") + ''
-        wrapProgram $out/bin/dolphin \
-            --set XDG_CONFIG_DIRS "${prev.libsForQt5.kservice}/etc/xdg:$XDG_CONFIG_DIRS" \
-            --run "${kprev.kservice}/bin/kbuildsycoca6 --noincremental ${prev.libsForQt5.kservice}/etc/xdg/menus/applications.menu"
-      '';
-    });
+    dolphin-wrapped = prev.writeShellScriptBin "dolphin" ''
+              export XDG_CONFIG_DIRS="${prev.libsForQt5.kservice}/etc/xdg:$XDG_CONFIG_DIRS"
+              ${kprev.kservice}/bin/kbuildsycoca6 --noincremental ${prev.libsForQt5.kservice}/etc/xdg/menus/applications.menu
+              exec ${kprev.dolphin}/bin/dolphin
+    '';
   });
 }
